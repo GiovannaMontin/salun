0523288553de89e26e2ab95ff2597a79bd240a6a
Montin <giovannamontinb@gmail.com>
2023-10-16 10:40:56 -0300

    Adição arquivo javascript

---
+++ forms.js
@@ -0,0 +1,13 @@
+
+
+// Declaração de variaveis - DOM
+
+    // Containers do Formulario
+        let section_forms = document.querySelector('.container_forms');
+        let section_cad = document.querySelector('#form_cadastro');
+        let section_login = document.querySelector('#form_login');
+
+    // Botões do Formulario
+        let btn_cad = document.querySelector('.btn_cadastro');
+        let btn_login = document.querySelector('.btn_login');
+
